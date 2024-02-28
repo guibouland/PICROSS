59e58e03e45d87717dd339389f613ecc1e2cda47
cmottier <camille.mottier@etu.umontpellier.fr>
2024-02-28 11:36:31 +0100

    Documentation fonction et description

--- R/Matrices.R
+++ R/Matrices.R
@@ -1,5 +1,11 @@
+#'matrice_alea crée une matrice carrée aléatoire de taille n contenant des 0 et 1, avec une proportion p de 1.
+#'
+#' @param n un entier
+#' @param p un réel de [0,1]
+#' @author Mottier
+#' @examples matrice_alea(5,0.6)
+#' @export
 matrice_alea <- function(n, p) {
-  #Créer une matrice aléatoire nxn avec une proportion p de 1
   M <- matrix(0, nrow = n, ncol = n)
   nb_noir <- floor(p*n^2)
   indices <- sample(1:(n*n), nb_noir)
@@ -7,8 +13,14 @@ matrice_alea <- function(n, p) {
   return(M)
 }
 
+#'compteur calcule le nombre de 1 successifs pour chaque ligne ou chaque colonne d'une matrice carrée comportant des 1 et des 0
+#'
+#' @param M une matrice carrée remplie de 0 et 1
+#' @param t un nombre prenant la valeur 1 pour compter par colonne
+#' @author Mottier
+#' @examples compteur(matrix(c(0,0,1,1,1,1,0,1,1,1,0,1,1,0,0,0),nrow=4,ncol=4,byrow=TRUE),0)
+#' @export
 compteur <- function(M,t) {
-  #t prend la valeur 0 pour compter par ligne et 1 pour compter par colonne
   if (t==1) {M <- t(M)}
   n <- nrow(M)
   res <- list()
@@ -18,7 +30,7 @@ compteur <- function(M,t) {
     for (j in 1:n) {
       if (M[i,j]==1) {
         compteur <- compteur + 1
-      } 
+      }
       if ((M[i,j]==0 & compteur!=0) | (M[i,j]==1 & j==n) ) {
         ligne_i <- append(ligne_i,compteur)
         compteur <- 0}
